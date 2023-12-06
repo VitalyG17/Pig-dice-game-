aa5c35c11211dab0f4271bd30137f7e73680fc54
VitayG <VitalyG-17@yandex.ru>
2023-12-06 16:59:56 +0300

    Добавлена логика подсчета очков, переход "активного игрока" и отображение игральной кости

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 'use strict';
+
+//Начальные значения очков и текущего игрока
+let currentScore = 0;
+let activePlayer = 0;
+const totalScores = [0, 0];
+
+//Выборка элементов
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const current0Elemet = document.getElementById('current--0');
+const current1Elemet = document.getElementById('current--1');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Установка изначальных значений
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
+//Смена игрока и обнуление очков
+const swithActivePlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
+
+btnRoll.addEventListener('click', function () {
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  //Отображение игральной кости
+  diceElement.classList.remove('hidden');
+  diceElement.src = `../Pig-dice-game-/img/dice${diceNumber}.png`;
+
+  // diceElement.classList.add('roll-animation-3d');
+  // setTimeout(function () {
+  //   diceElement.src = `../Pig-dice-game-/img/dice${diceNumber}.png`;
+
+  //   diceElement.classList.remove('roll-animation-3d');
+  // }, 1500);
+
+  if (diceNumber !== 1) {
+    //Подсчет очков для активного игрока
+    currentScore += diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else swithActivePlayer();
+});
+
+btnHold.addEventListener('click', function () {
+  totalScores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    totalScores[activePlayer];
+  swithActivePlayer();
+});
